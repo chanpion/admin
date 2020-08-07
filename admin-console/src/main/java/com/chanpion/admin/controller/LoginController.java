f44b06a2e7ccb57f511b3484b0ab8ead82d2dc7e
April.Chen <forchpeng@163.com>
2020-08-07 18:58:39 +0800

    shiro权限控制

--- admin-console/src/main/java/com/chanpion/admin/controller/LoginController.java
+++ admin-console/src/main/java/com/chanpion/admin/controller/LoginController.java
@@ -1,12 +1,19 @@
 package com.chanpion.admin.controller;
 
+import com.chanpion.admin.common.utils.LogUtil;
+import com.chanpion.admin.system.utils.CaptchaUtil;
 import org.apache.shiro.SecurityUtils;
-import org.apache.shiro.authc.AuthenticationException;
-import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.authc.*;
 import org.apache.shiro.authz.AuthorizationException;
+import org.apache.shiro.authz.UnauthorizedException;
 import org.apache.shiro.subject.Subject;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * @author April Chen
@@ -21,19 +28,40 @@ public class LoginController {
     }
 
     @PostMapping("login")
-    public String doLogin(String username, String password, @RequestParam(required = false, defaultValue = "false") Boolean rememberMe) {
-        Subject subject = SecurityUtils.getSubject();
+    public String doLogin(Model model, String username, String password,
+                          @RequestParam(required = false, defaultValue = "false") Boolean rememberMe) {
         UsernamePasswordToken token = new UsernamePasswordToken(username, password, rememberMe);
+        Subject subject = SecurityUtils.getSubject();
+        String msg = null;
         try {
             subject.login(token);
-        } catch (AuthenticationException e) {
-            e.printStackTrace();
-            return "/error";
-        } catch (AuthorizationException e) {
-            e.printStackTrace();
-            return "/403";
+            if (subject.isAuthenticated()) {
+                LogUtil.info("user {} login success.", username);
+                return "/";
+            } else {
+                return "error";
+            }
+        } catch (IncorrectCredentialsException e) {
+            msg = "密码有误，请重新输入!";
+        } catch (ExcessiveAttemptsException e) {
+            msg = "登录失败次数过多!";
+        } catch (LockedAccountException e) {
+            msg = "帐号已被锁定! ";
+        } catch (DisabledAccountException e) {
+            msg = "帐号已被禁用! ";
+        } catch (ExpiredCredentialsException e) {
+            msg = "帐号已过期! ";
+        } catch (UnknownAccountException e) {
+            msg = "帐号不存在!";
+        } catch (UnauthorizedException e) {
+            msg = "您没有得到相应的授权！" + e.getMessage();
+        } catch (Exception e) {
+            LogUtil.error("{}", e);
+        } finally {
+            model.addAttribute("message", msg);
+            LogUtil.info(msg);
         }
-        return "redirect:/";
+        return "/error";
     }
 
     /**
@@ -54,4 +82,14 @@ public class LoginController {
     public String forgotPassword() {
         return "forgot-password";
     }
+
+    /**
+     * 验证码
+     */
+    @RequestMapping("/captcha")
+    @ResponseBody
+    public void captcha(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        CaptchaUtil.captcha(request, response);
+    }
+
 }
---
+++ admin-system/src/main/java/com/chanpion/admin/system/utils/ShiroUtil.java
@@ -0,0 +1,52 @@
+package com.chanpion.admin.system.utils;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.crypto.SecureRandomNumberGenerator;
+import org.apache.shiro.crypto.hash.SimpleHash;
+import org.apache.shiro.util.ByteSource;
+
+/**
+ * @author April Chen
+ * @date 2019/9/6 16:20
+ */
+public class ShiroUtil {
+    private static SecureRandomNumberGenerator secureRandom = new SecureRandomNumberGenerator();
+    public static final String ALGORITHM_NAME = "md5";
+    public static final int HASH_ITERATIONS = 2;
+
+    /**
+     * 加密密码
+     *
+     * @param password 原始密码
+     * @return 加密后密码
+     */
+    public String encryptPassword(String password) {
+        return encryptPassword(password, randomSalt());
+    }
+
+    /**
+     * 加密密码
+     *
+     * @param password 原始密码
+     * @param salt     加密盐
+     * @return 加密后密码
+     */
+    public static String encryptPassword(String password, String salt) {
+        if (StringUtils.isBlank(salt)) {
+            salt = randomSalt();
+        }
+        return new SimpleHash(ALGORITHM_NAME, password, ByteSource.Util.bytes(salt), HASH_ITERATIONS).toHex();
+    }
+
+    private static String randomSalt() {
+        return secureRandom.nextBytes().toHex();
+    }
+
+    /**
+     * 是否登录状态
+     */
+    public static boolean isLogin() {
+        return SecurityUtils.getSubject().getPrincipal() != null;
+    }
+}
