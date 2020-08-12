202f759ba616918c45917ffa2eb007004640951d
April.Chen <forchpeng@163.com>
2020-08-12 20:02:49 +0800

    集成Aspect切面

--- admin-system/src/main/java/com/chanpion/admin/system/aop/ServiceAspect.java
+++ admin-system/src/main/java/com/chanpion/admin/system/aop/ServiceAspect.java
@@ -8,17 +8,19 @@ import org.aspectj.lang.annotation.Pointcut;
 
 /**
  * @author April Chen
- * @date 2020/8/11 13:52
+ * @date 2020/8/12 19:55
  */
 @Aspect
-public class DaoAspect {
+public class ServiceAspect {
 
-    @Pointcut("execution(* com.chanpion.admin.system.dao.*.*(..))")
-    public void pointcut() {
+    @Pointcut("execution(* com.chanpion.admin.system.service.*.*(..))")
+    public void pointcut(){
+
     }
 
     @Around("pointcut()")
     public Object around(ProceedingJoinPoint joinPoint) throws Throwable {
+        System.out.println("aop");
         long begin = System.currentTimeMillis();
         String name = joinPoint.getSignature().getName();
         Object result;
--- admin-system/src/main/java/com/chanpion/admin/system/entity/User.java
+++ admin-system/src/main/java/com/chanpion/admin/system/entity/User.java
@@ -1,14 +1,11 @@
 package com.chanpion.admin.system.entity;
 
-import lombok.Data;
-
 import java.util.Date;
 
 /**
  * @author April Chen
  * @date 2020/7/28 22:36
  */
-@Data
 public class User {
     private Long id;
     private String username;
@@ -18,4 +15,68 @@ public class User {
     private Date createTime;
     private Integer age;
     private String salt;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getSalt() {
+        return salt;
+    }
+
+    public void setSalt(String salt) {
+        this.salt = salt;
+    }
 }
